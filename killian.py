c5eae93320f904bc658ada519e6d3a639ff1a0f1
Noémie Galliot <noemie.galliot@free.fr>
2024-10-29 16:28:43 +0100

    rajout d'une commande pour lecture
    J'ai rajouté une commande pour ignorer les premières lignes inutiles du documents csv

--- killian.py
+++ killian.py
@@ -2,7 +2,7 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
-df = pd.read_csv('PS_2024.10.29_06.08.28.csv', index_col=0)
+df = pd.read_csv('PS_2024.10.29_06.08.28.csv', index_col=0,skiprows =291)
 
 #Mass en fonction du rayon
 a = df[["pl_masse","pl_rade","tran_flag"]][df["tran_flag"] == 1]
