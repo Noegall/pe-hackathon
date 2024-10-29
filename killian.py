8c80a27aec4b021063506ac080df0b7453ab4591
Arnaud Deltour <arnaud.deltour@etu.minesparis.psl.eu>
2024-10-29 16:14:19 +0100

    Calcul et graphe du nombre de découvertes par an

--- killian.py
+++ killian.py
@@ -2,7 +2,7 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
-df = pd.read_csv('PS_2024.10.29_06.08.28.csv', index_col=0)
+df = pd.read_csv('PS_2024.10.29_06.08.28.csv', index_col=0, skiprows=291)
 
 #Mass en fonction du rayon
 a = df[["pl_masse","pl_rade","tran_flag"]][df["tran_flag"] == 1]
@@ -31,8 +31,23 @@ df = df.drop("ima_flag",axis="columns")
 df = df.drop("dkin_flag",axis="columns")
 df = df.drop_duplicates()
 
-# -
+# +
+dico = {}
+df1 = df['disc_year']
+for c in df1:
+    if c in dico:
+        dico[c] += 1
+    else:
+        dico[c] = 1
 
+
+df2 = pd.DataFrame(dict(years = list(dico.keys()),number = list(dico.values())))
+df2 = df2.sort_values('years')
+
+plt.bar(years, number, data=df2, tick_label=years, title="Nombre d'exoplanètes découvertes par années")
+plt.xticks(rotation=60, ha='right')
+plt.show()
+# -
 
 
 
